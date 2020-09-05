f917097b1cc2c413ae36dbc2bb1cf5505f2e98d2
X-Classified <chaikohen@gmail.com>
2020-09-05 03:58:25 -0700

    change initial settings for plati discord

--- seeds/settings.js
+++ seeds/settings.js
@@ -3,57 +3,38 @@
 module.exports = [
   ['app_operator', 'platicorn'],
 
-  ['discord_activity', 'over all the cuties :)'],
+  ['discord_activity', 'the entrance hall'],
   ['discord_activity_type', 'WATCHING'],
   /* DISCORD CHANNELS */
   // TEST SERVER
-  ['discord_channel_commandManagement', '742651722291347497'],
-  ['discord_channel_alerts', '742651473158209577'],
-  ['discord_channel_console', '743065516951994429'],
-  ['discord_channel_whispers', '742651437368213605'],
-  ['discord_channel_music', '743062887198752849'],
+  ['discord_channel_commandManagement', '745931568572399646'],
+  ['discord_channel_alerts', '541318953088188456'],
+  ['discord_channel_console', '745931477853798521'],
+  ['discord_channel_whispers', '745931477853798521'],
+  ['discord_channel_music', '745932185743261737'],
 
   ['discord_music_volume', '1'],
-  /* TEST SERVER WEBHOOKS */
-  // Twitch
-  /*
-  ['discord_webhook_ban', '735603870662656091/ISoYCV3RCzn9R92QakzqVJt-M3VANSAYHCqw6-mqkq15N67veqNzBrkvhUPoxagCWOe9'],
-  ['discord_webhook_delete', '735603958596370472/icy5VS_rYLYaIfxvU9lG3nJyLFt8oU0yP_3ig-9nrFGLexSW2BIyOlxeccYlLeADKERr'],
-  ['discord_webhook_review', '735604113731092630/mcBwMQiWrm7ccMwLjTk8PVqknbls1GStOSCg8it7_Nr-6-ZnHE_Ct8pueAyojzmcf2-i'],
-  ['discord_webhook_timeout', '735602651693056043/Z9SoCY1g259WuBPZ94kdGrN7KSe1ew6BeHL4fNDNZUolLhsAthPFLTMH9nLG3uotWRxB'],
-  ['discord_webhook_unban', '735604051181305956/3F7vFD1LTx37mr8U73Bcn8mSYxwf6X8m6VSr7ODSURhd9tE7HCdIStn_V0-u38P1x-l4'],
-  // Discord
-  ['discord_webhook_clear', '741993554603409438/yVSLxfIWCVVHx8B8mnilCwCSKJoJxDN9zPYlrVLNPSbCbRhCjICBTn-2dVBlvM8K7XCJ'],
-  ['discord_webhook_msgEdit', '742212795164000357/Vr3s_7HeTXiwBVRCj48_AmHXYsbrdn6yRMhtTAgq_-qmuzgt-__jcVyJOS1NZZOha-cW'],
-  ['discord_webhook_msgDelete', '742213417112174672/9gMCokW86ruXjOt2rmTBo878Zi3-dxzxEzjLuBimE3CIZtlD98hf9QTfz7a-y7pugOf_'],
-  ['discord_webhook_userJoin', '742214411497898147/GWFzhLtkB5F_ZaGFalUvh2YYIYdTyMv-LNtoLYyLa6AjbntlzJr0Nm91SM7ADcb4WO2w'],
-  ['discord_webhook_userLeft', '742214591781797918/9K9ZpaHgsa2ku9jmH22qDIBsW-MhYyMS6tVQSPratLzOdlx2YKkAs3xwi_vzlfiw7mxy'],
-  ['discord_webhook_userBan', '742214643162021930/0htYR8CG3oydcFOYZk64D1dqGEd1TZrZnQ4FPk0BeQVHdSkSYJ4ru7x8-B20pV2mLL94'],
-  ['discord_webhook_roleUpdate', '742214696471363656/0rYtL_jm21-8U3iJc6SJOo3akKgZeog-aiqBlAydnhHXcbs5xNVNwQ6KulPzV7Fh7KKL'],
-  ['discord_webhook_nickChange', '742214793594929322/_kxd_WKH0LrnkclAzbVROc9-9oRWVgM4rhSRTJAsMBsZjHBTqudxlvjRu1UCcrumIsRm'],
-  */
 
-  /* SHANTY WEBHOOKS */
   // Twitch
-  ['discord_webhook_ban', '742677319755366411/ule0wDuiU2ejHhvdE09bnKHWq0x9JzB8ECywLUtRl2766waWGNxO7b5pWRyFgkOO1jK6'],
-  ['discord_webhook_delete', '742677536240042067/_xl_ncQ-PGXXnE-E7v6kMiW4Lf12BrxiLP8u7aSj7o-g-3Qa9GkkTWmP9ETRp1gdLZ-c'],
-  ['discord_webhook_review', '742677570147057784/lP9gfBI6vCPUOlD1ySEXQ3H6wGbb40bYiy0G0RdpznKS3Ek_0G43tWRHTl4ZQED9eY1p'],
-  ['discord_webhook_timeout', '742677243083489300/S4mJiH8djxi31TIJUDJULmPULGs5W8hsmZaxkCpI-fe_xCOj3DjkOKKgkfpMeHQBBdas'],
-  ['discord_webhook_unban', '742677339758133290/xbhg7R6FS6aEPuUO7cHbihuXgQi2xkYuhSZLTc72-Mmr7VrAotM15scxYKRR-919C1Tj'],
+  ['discord_webhook_ban', ''],
+  ['discord_webhook_delete', ''],
+  ['discord_webhook_review', ''],
+  ['discord_webhook_timeout', ''],
+  ['discord_webhook_unban', ''],
   // Discord
-  ['discord_webhook_clear', '742680186658488372/Wnn0gLj2yBdeOJfVHIYQNtu63R9qKFpQj1RXGi5W-m2_PorQ_-7VWzvUrUolC5aEkm8t'],
-  ['discord_webhook_msgEdit', '742680222096162896/yGoFbaMyA6-JaC3X24MwtDVwMOpruQaYlBu5AnIUoPT8mmDAYK5lDxAdZHPX0WQjk5gI'],
-  ['discord_webhook_msgDelete', '742680262520864858/W41MUg-OBcNdJbBclDzGEFXBvQOaw3CpAMi7_JBNpGZZNP5vye_kgVMQflgTIZc75VfD'],
-  ['discord_webhook_userJoin', '742680286986240020/ZzomF52eSWAO8F--9SE-rE1qAmf1SlMk5YAk46tVchdxeiu9qFj7osm8dzCYaOZjPU_W'],
-  ['discord_webhook_userLeft', '742680326370754590/HvhAVZeIRbFwPMUSlucuXx2xt2F0_C_ULfsP1av_DD6372q2mnp75FtbdlGbojUPP-z1'],
-  ['discord_webhook_userBan', '742680351746424863/gig84p0saPBFbIv3e_HNYKazPn81yRuFd3-YCUfoNEkOvPpm1HNyY9A5wbznnTco9qTl'],
-  ['discord_webhook_roleUpdate', '742893842931384452/8hKVQjC2ZKEE4RfYcpFOPL1JqZARVwcL0eAHLa63Ikwyykdu40jvVQrSY7zGbmIAzz5c'],
-  ['discord_webhook_nickChange', '742680372394852352/uJ-PHdra_FWOeyULj2J0GCNjXBpOk6urlPyzQvd62jMTXZ3YPJVswWDG5dEXCoW4fTGi'],
+  ['discord_webhook_clear', ''],
+  ['discord_webhook_msgEdit', ''],
+  ['discord_webhook_msgDelete', ''],
+  ['discord_webhook_userJoin', ''],
+  ['discord_webhook_userLeft', ''],
+  ['discord_webhook_userBan', ''],
+  ['discord_webhook_roleUpdate', ''],
+  ['discord_webhook_nickChange', ''],
 
   /* IRC MESSAGES */
-  ['irc_message_cheer', '/me {user} cheered {amount} snazjdLove !'],
+  ['irc_message_cheer', '/me {user} cheered {amount} platicLove !'],
   ['irc_message_resub', '/me {user} resubbed for {duration}!'],
-  ['irc_message_stream_down', '/me Thanks for watching snazjdLove !'],
+  ['irc_message_stream_down', '/me Thanks for watching platicLove !'],
   ['irc_message_stream_up', '/me Welcome to the stream early gang!'],
-  ['irc_message_sub', '/me {user} just subbed! Welcome to the Snazzy Shanty snazjdPog !'],
+  ['irc_message_sub', '/me {user} just subbed! Welcome to the Corn Crew platicPog !'],
 ];
