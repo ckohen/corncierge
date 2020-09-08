d7354ade8ea50dd28b4e2048c5953191f5f88dd4
X-Classified <chaikohen@gmail.com>
2020-09-08 00:45:22 -0700

    Make default help embed more friendly

--- app/discord/embeds/help.js
+++ app/discord/embeds/help.js
@@ -1,4 +1,4 @@
 module.exports = (comp, prefix) => comp
       .setColor('red')
-      .setTitle("Help")
+      .setTitle("Hello, Corncierge Speaking, how can I help you today?")
       .setDescription("If a command is not listed here, see the full help for that command by using `" + prefix + "help legacy <command>`");
