8b7ea397744309f2363a71c2c81d7e58e9f39643
X-Classified <chaikohen@gmail.com>
2020-09-14 19:41:25 -0700

    Need to recache managers

--- app/discord/events/guildCreate.js
+++ app/discord/events/guildCreate.js
@@ -1,12 +1,31 @@
 'use strict';
 
+const { collect } = require('../../util/helpers');
+
 module.exports = (socket, guild) => {
     socket.app.log.out('info', module, "Joined new server: " + guild.name)
 
     // Add new guild to role and color managers
-    socket.app.database.addColorManager(String(guild.id));
-    socket.app.database.addRoleManager(String(guild.id));
-    socket.app.database.addPrefix(String(guild.id));
+    await socket.app.database.addColorManager(String(guild.id));
+    await socket.app.database.addRoleManager(String(guild.id));
+    await socket.app.database.addPrefix(String(guild.id));
+
+    // Re-cache managers
+    await socket.app.database.getRoleManager().then((all) => {
+        socket.roleManager.clear();
+        collect(socket.roleManager, all, "guildID", false);
+      });
+  
+      await socket.app.database.getColorManager().then((all) => {
+        socket.colorManager.clear();
+        collect(socket.colorManager, all, "guildID", false);
+      });
+  
+      await socket.app.database.getPrefixes().then((all) => {
+        socket.prefixes.clear();
+        collect(socket.prefixes, all, "guildID", false);
+      });
+  
 
     // Send info message in system channel
 
