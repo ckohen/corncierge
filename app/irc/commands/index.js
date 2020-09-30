3ee0a7fa1dd2e432cff2ee91635f4a9fb4ea40bc
X-Classified <chaikohen@gmail.com>
2020-09-30 03:54:40 -0700

    Add additional command overides

--- app/irc/commands/index.js
+++ app/irc/commands/index.js
@@ -5,5 +5,6 @@ const commands = {};
 commands.joke = require('./joke');
 commands.uptime = require('./uptime');
 commands.followage = require('./followage');
+commands.poki = require('./poki');
 
 module.exports = commands;
---
+++ app/irc/commands/poki.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = (socket, callback) => {
+    callback("poki1 poki2");
+    callback("poki3 poki4");
+};
