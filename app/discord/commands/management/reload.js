760ec858571c2c4beca8e75ae4aab422c136bbc0
X-Classified <chaikohen@gmail.com>
2020-09-30 14:04:27 -0700

    Update to reload all database values

--- app/discord/commands/management/reload.js
+++ app/discord/commands/management/reload.js
@@ -7,7 +7,7 @@ module.exports = {
   channel: 'console',
 
   async run(socket, message) {
-    const { irc, log, settings } = socket.app;
+    const { discord, irc, log, settings, streaming } = socket.app;
 
     log.out('info', module, 'Reload instruct received');
 
@@ -17,15 +17,20 @@ module.exports = {
     // Reload application state
     Promise.all([
       socket.app.setSettings(),
+      socket.app.setStreaming(),
       irc.setCache(),
+      discord.setCache(),
     ]).then(async () => {
       const md = (name, metric) => `**${metric}** ${plural(name, metric)}`;
       const response = [
         'Reload complete.',
         `>>> ${md('settings', settings.size)}`,
+        md('streaming users', streaming.size),
         md('filters', irc.filters.size),
         md('commands', irc.commands.size),
         md('jokes', irc.jokes.length),
+        md('music guilds', discord.musicData.size),
+        md('total guilds', discord.prefixes.size),
       ];
 
       await socket.driver.user.setActivity(socket.app.settings.get('discord_activity') || null, {type: socket.app.settings.get('discord_activity_type') || 'PLAYING'});
