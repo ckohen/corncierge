e7454c1ce86ed080c8c5cd930bd31508d6095c83
X-Classified <chaikohen@gmail.com>
2020-10-08 00:56:40 -0700

    add guild specifc member join handling

--- app/discord/events/guildMemberAdd.js
+++ app/discord/events/guildMemberAdd.js
@@ -10,9 +10,13 @@ module.exports = (socket, member) => {
     member.user.id,
   ]);
   if (socket.isGuild(member.guild.id, 'platicorn')) {
+    member.roles.add("581396312914919424");
     socket.sendWebhook('userJoin', embed,);
   }
   else if (member.guild.id === "756319910191300778") {
     socket.sendMessage('helpLogs', embed,);
+  }
+  else if (member.guild.id === "140230462940381184") {
+    setTimeout(function() { member.roles.add("140254897479090176")}, 300000);
   }
 };
