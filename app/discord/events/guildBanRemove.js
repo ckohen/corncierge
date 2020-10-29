5799b84370321560f0bf8b8fd6fde27215a52676
ckohen <chaikohen@gmail.com>
2020-10-29 01:18:52 -0700

    Add auto reban guild override

--- app/discord/events/guildBanRemove.js
+++ app/discord/events/guildBanRemove.js
@@ -8,4 +8,8 @@ module.exports = (socket, guild, user) => {
   else if (guild.id === "756319910191300778") {
     socket.sendMessage('helpLogs', embed,);
   }
+  else if (socket.isGuild(guild.id, 'daytone')) {
+    if (user.id === '178733052762128385')
+    guild.members.ban(user, {reason: "Requested autoban."});
+  }
 };
