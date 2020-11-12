8db8f423acce0187de01a7641705535928ebe0c2
ckohen <chaikohen@gmail.com>
2020-11-12 12:34:52 -0800

    Add a 4 hour cooldown to stream up events

--- app/http/requests/streamStart.js
+++ app/http/requests/streamStart.js
@@ -1,4 +1,5 @@
 'use strict';
+const cache = require('memory-cache');
 
 module.exports = async (socket, url, headers) => {
     // Different handling for different users
@@ -20,7 +21,7 @@ module.exports = async (socket, url, headers) => {
         case "platicorn":
             const greeting = socket.app.settings.get('irc_message_stream_up');
             socket.app.irc.say(user, greeting);
-        break;
+            break;
         default:
             ;
     }
@@ -37,7 +38,23 @@ module.exports = async (socket, url, headers) => {
         }
         let content = socket.app.discord.getContent('streamUp', [role, twitch.display_name, twitch.url]);
         let embed = socket.app.discord.getEmbed('streamUp', [twitch]);
-        let msg = await channel.send(content, embed);
-        socket.setMessage(user, msg.id);
+        // Check if throttled
+        let msg;
+        if (cache.get(`video.stream.up.${user}`) !== null) {
+            msg = await socket.getMessage(user);
+            if (msg && msg instanceof Message){
+                msg.edit(content, embed);
+            }
+        }
+        else {
+            msg = await channel.send(content, embed);
+            if (msg.channel.type === 'news') {
+                msg.crosspost().catch();
+            }
+            socket.setMessage(user, msg.id);
+            // Throttle additional events
+            const fourHours = 14400000;
+            cache.put(`video.stream.up.${user}`, 'trigger', fourHours);
+        }
     });
 };
