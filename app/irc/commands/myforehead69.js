91d635921ab682c42f9d78b60f1a861e60a0cd68
ckohen <chaikohen@gmail.com>
2020-11-18 13:36:12 -0800

    fix(IRC-commands): Make myforehead69 first message shorter so it doesn't auto-split

--- app/irc/commands/myforehead69.js
+++ app/irc/commands/myforehead69.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = (socket, callback) => {
-    callback("Ok I’m getting away from the odyssey community, you all are truly evil, eerily similar to the Nazis. Especially lucidityy. Everyone in the community is a degenerate. Chaospringle is 20 years old, obese and lives with his mom. Mitch_smo is an annoying weeb. These people make me scared for the future. And you, Lindsey, have NO redeeming qualities. You’re not smart enough to get into a smart school, you suck at odyssey, I mean, you have had a 1:08 in odyssey after playing for a year. A YEAR. I got 1:08 in about 6 runs.");
+    callback("Ok I’m getting away from the odyssey community, you all are truly evil, eerily similar to the Nazis. Especially lucidityy. Everyone in the community is a degenerate. Chaospringle is 20 years old, obese and lives with his mom. Mitch_smo is an annoying weeb. These people make me scared for the future. And you, Lindsey, have NO redeeming qualities.");
+    callback("You’re not smart enough to get into a smart school, you suck at odyssey, I mean, you have had a 1:08 in odyssey after playing for a year. A YEAR. I got 1:08 in about 6 runs.");
     callback("Nobody wants to watch a bad speedrunner with a bad personality. Your only career is going to be onlyfans. And you have so much potential. A beautiful young lady playing video games.");
     callback("And also, lucidityy definitely has a crush on you."); 
     callback("I’d recommend you have sex with him.");
