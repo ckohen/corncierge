6bc6479cead363b027077636d5fd6b6e27efc24c
ckohen <chaikohen@gmail.com>
2020-11-18 13:32:14 -0800

    feat(IRC-commands): Add myforehad69 command

--- app/irc/commands/index.js
+++ app/irc/commands/index.js
@@ -6,5 +6,6 @@ commands.joke = require('./joke');
 commands.uptime = require('./uptime');
 commands.followage = require('./followage');
 commands.poki = require('./poki');
+commands.myforehead69 = require('./myforehead69');
 
 module.exports = commands;
