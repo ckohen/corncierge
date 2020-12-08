7f7e8603ef66fc359d75dc476b98c40ae8c4d08d
ckohen <chaikohen@gmail.com>
2020-12-08 14:27:40 -0800

    feat(commands): add jumbo to command listing

--- app/discord/commands/moderation/index.js
+++ app/discord/commands/moderation/index.js
@@ -9,5 +9,6 @@ commands.moveall = require('./moveall');
 commands.prefix = require('./prefix');
 commands.randommove = require('./randommove');
 commands.nuke = require('./nuke');
+commands.jumbo = require('./jumbo');
 
 module.exports = commands;
