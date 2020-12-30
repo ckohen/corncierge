001b27abc5c1af3163404ffead6d0b3913f4c95a
ckohen <chaikohen@gmail.com>
2020-12-30 02:49:36 -0800

    feat(irc): remove dependency on internal reauth

--- app/irc/events/disconnected.js
+++ app/irc/events/disconnected.js
@@ -4,8 +4,4 @@ module.exports = async (socket, reason) => {
   if (!socket.app.ending) {
     socket.app.log.warn(module, `Disconnected: ${reason}, attempting reconnect...`);
   }
-  if (reason === 'Login authentication failed') {
-    const newToken = await socket.app.auth.refreshToken(socket.app.options.irc.identity.username);
-    socket.setDriver(newToken);
-  }
 };
