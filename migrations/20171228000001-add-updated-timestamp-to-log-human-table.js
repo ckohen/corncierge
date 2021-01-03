0ef40ec79e3fc18a3dc844825aa1b15a3f7725c7
ckohen <chaikohen@gmail.com>
2021-01-03 03:31:50 -0800

    feat(Managers): remove outdated table info

--- migrations/20171228000001-add-updated-timestamp-to-log-human-table.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.up = (db) => db.addColumn('log_human', 'updated_at', {
-  type: 'timestamp', notNull: false, defaultValue: null,
-});
-
-exports.down = (db) => db.removeColumn('log_human', 'updated_at');
--- migrations/20181207000001-add-message-to-log-human-table.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.up = (db) => db.addColumn('log_human', 'message', {
-  type: 'text', notNull: false, defaultValue: null,
-});
-
-exports.down = (db) => db.removeColumn('log_human', 'message');
