f1a25ad2c0c115624f3f139fcd14e463189e5d9a
ckohen <chaikohen@gmail.com>
2021-01-08 00:43:08 -0800

    docs: add docstring for HTTPUtil

--- app/util/HTTPUtil.js
+++ app/util/HTTPUtil.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * Stores various http specific utilities
+ */
 class HTTPUtil {
   constructor() {
     throw new Error(`The ${this.constructor.name} class may not be instantiated.`);
