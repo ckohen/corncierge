7fe9b4f96bb07b1da0fa9273a270f871644cd558
ckohen <chaikohen@gmail.com>
2021-03-24 13:09:17 -0700

    feat(InviteCommand): add applications.commands to invite

--- app/discord/commands/general/invite.js
+++ app/discord/commands/general/invite.js
@@ -13,7 +13,8 @@ class InviteCommand extends BaseCommand {
   run(socket, message) {
     message.channel.send(
       'Thank you for your interest in corncierge, you can add the bot and see more infomation at <https://www.corncierge.com> \n\n' +
-        'and here is a direct invite link: \n<https://discord.com/oauth2/authorize?client_id=745920843690803211&permissions=1073212534&scope=bot>',
+        'and here is a direct invite link: \n' +
+        '<https://discord.com/oauth2/authorize?client_id=745920843690803211&permissions=1073212534&scope=bot%20applications.commands>',
     );
   }
 }
