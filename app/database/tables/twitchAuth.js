36b4c8e17f73999a069fc06c244f483bc2057762
ckohen <chaikohen@gmail.com>
2022-01-24 02:29:27 -0800

    feat(database): support twitchlibs auth database format

--- app/database/tables/twitchAuth.js
+++ app/database/tables/twitchAuth.js
@@ -16,6 +16,16 @@ class twitchAuthTable extends BaseTable {
   }
 
   /**
+   * Get a single authenticated users
+   * @param {string} id the id of the user to get tokens for
+   * @returns {Promise<Object>}
+   */
+  getSingle(id) {
+    const data = this.socket.query('SELECT id, accessToken, refreshToken, scopes FROM `twitchauth` WHERE `id` = ?', [id]);
+    return this.parseJSON(['scopes'], data)[0];
+  }
+
+  /**
    * Add an authenticated user
    * @param {string} id The id of the user this token belongs to
    * @param {string} access The access token for this user
@@ -30,6 +40,35 @@ class twitchAuthTable extends BaseTable {
       refresh,
       JSON.stringify(scopes),
     ]);
+  }
+
+  /**
+   * Add or update an authenticated user
+   * @param {string} id The id of the user this token belongs to
+   * @param {string} access The access token for this user
+   * @param {string} refresh The refresh token for this user
+   * @param {Object|Array} scopes The scopes that are authorized for this user
+   * @returns {Promise<void>}
+   */
+  upsert(id, access, refresh, scopes) {
+    return this.socket.query(
+      'INSERT INTO `twitchauth` (id, accessToken, refreshToken, scopes) VALUES (?, ?, ?, ?) ' +
+        'ON DUPLICATE KEY UPDATE `accessToken` = VALUE(accessToken), `refreshToken` = VALUE(refreshToken), `scopes` = VALUE(scopes)',
+      [id, access, refresh, JSON.stringify(scopes)],
+    );
+  }
+
+  /**
+   * Add or update an authenticated user
+   * @param {Object[]} data the data for all the users to upsert
+   * @returns {Promise<void>}
+   */
+  upsertMultiple(data) {
+    return this.socket.query(
+      'INSERT INTO `twitchauth` (id, accessToken, refreshToken, scopes) VALUES ? ' +
+        'ON DUPLICATE KEY UPDATE `accessToken` = VALUE(accessToken), `refreshToken` = VALUE(refreshToken), `scopes` = VALUE(scopes)',
+      [data.map(auth => `(${auth.id}, ${auth.accessToken}, ${auth.refreshToken}, ${auth.scopes})`).join(',')],
+    );
   }
 
   /**
